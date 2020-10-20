485ecc533d6ce4e43258ea6d522d92303094a6aa
JacobM184 <55066056+JacobM184@users.noreply.github.com>
2020-10-20 20:54:07 +1300

    Update values.h

--- values.h
+++ values.h
@@ -1,18 +1,34 @@
 /*
- * values.h
+ * value.h
  *
  *  Created on: 18/10/2020
- *      Author: jbuq319
+ *      Author: Biggie Cheese
  */
 
-#ifndef VALUES_H_
-#define VALUES_H_
+#ifndef VALUE_H_
+#define VALUE_H_
+
+#define TRUE 1
+#define FALSE 0
 
 #define AVI_VALUE 300
-#define AEI_VALUE 800
-#define PVARP_VALUE 50
 #define VRP_VALUE 150
+#define PVARP_VALUE 50
+#define AEI_VALUE 800
 #define LRI_VALUE 950
 #define URI_VALUE 900
 
-#endif /* VALUES_H_ */
+#define AVI_LED 0x1
+#define PVARP_LED 0x2
+#define VRP_LED 0x4
+#define AEI_LED 0x8
+#define LRI_LED 0x10
+//#define LRI_B_LED 0x20
+#define URI_LED 0x20
+//#define URI_B_LED 0x80
+
+// define UART status bits
+#define IRRDY 0x40
+#define TRDY 0x20
+
+#endif /* VALUE_H_ */
