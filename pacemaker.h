267bdbebb85b53de995e8d1b83ed03385117d4ad
JacobM184 <55066056+JacobM184@users.noreply.github.com>
2020-10-20 20:54:38 +1300

    Create pacemaker.h

---
+++ pacemaker.h
@@ -0,0 +1,45 @@
+/*
+ * pacemaker.h
+ *
+ *  Created on: 19/10/2020
+ *      Author: Biggie Cheese
+ */
+
+#ifndef PACEMAKER_H_
+#define PACEMAKER_H_
+
+//FUNCTIONS
+void tick();
+void reset();
+
+//INPUT
+extern char R;
+extern char ASense;
+extern char VSense;
+
+extern char PVARP_ex;
+extern char AVI_ex;
+extern char URI_ex;
+extern char LRI_ex;
+extern char VRP_ex;
+extern char AEI_ex;
+
+//OUTPUT
+extern char VPace;
+extern char APace;
+
+extern char AVI_stop;
+extern char AEI_stop;
+extern char LRI_stop;
+extern char VRP_stop;
+extern char PVARP_stop;
+extern char URI_stop;
+
+extern char VRP_start;
+extern char PVARP_start;
+extern char URI_start;
+extern char AVI_start;
+extern char AEI_start;
+extern char LRI_start;
+
+#endif /* PACEMAKER_H_ */
